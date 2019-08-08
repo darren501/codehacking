3ed00ed3083c3009b3a1fa8785ab99b2c3849571
Kevin <kevindarrencooper@outlook.com>
2019-08-08 13:02:40 -0400

    Admin Master Page Setup & Stylesheets Using Node

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/libs.css')}}">
+    <title>Admin Users Index</title>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Administrator Page</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor..</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor..</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3> 
+      <p>Lorem ipsum dolor..</p>
+    </div>
+  </div>
+</div>
+    
+</body>
+</html>
